012d91a1a1feca9a819f06e1b9c02b39f1b94e38
Plakhtiev <monatkotata@gmail.com>
2021-03-18 12:17:00 +0200

    clear main

--- main.cpp
+++ main.cpp
@@ -23,21 +23,5 @@ int main()
 	ICoffeeMachine& machine = CreateCoffeeMachine();
 
 	machine.MakeAmericano();
-
-	CoffeeMachine mach(1000, 1000, 1000);
-
-	Recipe* americano;
-	americano = new Recipe(20, 1, 0, "americano");
-
-	Recipe* latte;
-	latte = new Recipe(180, 6, 80, "latte");
-
-	mach.SetRecipe(americano);
-	mach.SetRecipe(latte);
-	mach.MakeAmericano();
-	mach.MakeLatte();
-
-	std::cout << "GetWater - " << mach.GetWater() << std::endl;
-	std::cout << "GetMilk - " << mach.GetMilk() << std::endl;
-	std::cout << "GetSugar - " << mach.GetSugar() << std::endl;
+	
 }
